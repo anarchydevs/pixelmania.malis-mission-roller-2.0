620123c71e65cb5e516503c3dcdc6cd51e18f574
Pixelmania <84938061+Malishade@users.noreply.github.com>
2022-06-24 09:39:36 +0200

    resolution fix attempt

--- Main.cs
+++ Main.cs
@@ -29,9 +29,9 @@ namespace MaliMissionRoller2
             //_window.Show();
             //_window.Window.MoveTo(Settings.Frame.X, Settings.Frame.Y);
             //Game.OnUpdate += Update;
-            Mission.RollListChanged += RollListChanged;
-            Network.N3MessageSent += N3Message_Sent;
-            Game.TeleportEnded += Game_OnTeleportEnded;
+           // Mission.RollListChanged += RollListChanged;
+            //Network.N3MessageSent += N3Message_Sent;
+           // Game.TeleportEnded += Game_OnTeleportEnded;
             //Sounds = new Sounds();
             //Extensions.PlaySound(Sounds.Alert);
             Chat.RegisterCommand("mmr", (string command, string[] param, ChatWindow chatWindow) => DevCmd(param));
